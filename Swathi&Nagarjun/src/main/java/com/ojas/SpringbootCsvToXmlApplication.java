f90f512f2f48368fe144208baad78626cdeee77f
Swathi Kommoju <sk19305@corp.ojas-it.com>
2020-10-09 19:16:03 +0530

    POC3 Added By Swathi & Nagarjun

---
+++ Swathi&Nagarjun/src/main/java/com/ojas/SpringbootCsvToXmlApplication.java
@@ -0,0 +1,14 @@
+package com.ojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringbootCsvToXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCsvToXmlApplication.class, args);
+	}
+
+}
---
+++ Swathi&Nagarjun/src/main/java/com/ojas/processor/EmployeeProcessor.java
@@ -0,0 +1,14 @@
+package com.ojas.processor;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.ojas.model.Employee;
+
+public class EmployeeProcessor implements ItemProcessor<Employee, Employee>{
+
+	@Override
+	public Employee process(Employee item) throws Exception {
+		return item;
+	}
+
+}
