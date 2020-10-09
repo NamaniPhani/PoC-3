8deb660cd6097a2bc66b28315545b2fed9a41564
Veera Lakshmi Supraja Palagi <plakshmi@corp.ojas-it.com>
2020-10-09 19:03:39 +0530

    poc3 successfully added by supraja & Rohit

---
+++ poc6-springbatch(supraja&Rohit)/src/main/java/com/ojas/Poc6SpringbatchApplication.java
@@ -0,0 +1,15 @@
+package com.ojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+// (exclude = DataSourceAutoConfiguration.class)
+public class Poc6SpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc6SpringbatchApplication.class, args);
+	}
+
+}
