a70318bf6fe719ebb8fee908d2bd05bc95b2732c
pranaypodduturi1 <pranay.podduturi@ojas-it.com>
2020-10-10 10:17:37 +0530

    pranay&ganesh code

---
+++ pranay&ganesh/src/main/java/com/ojas/task/SpringBatchPocApplication.java
@@ -0,0 +1,14 @@
+package com.ojas.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringBatchPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchPocApplication.class, args);
+	}
+
+}
